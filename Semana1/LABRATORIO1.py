19654c8e2f30e9afbd3d42c79115dfacb25ce10f
LAPTOP CATALINA <catalinarjs.172@gmail.com>
2025-01-26 22:15:41 -0600

    Create LABORATORIO1.py

---
+++ Semana1/LABRATORIO1.py
@@ -0,0 +1,26 @@
+# 1 ¿Cuál es la función de la memoria secundaria en una computadora?
+#   Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido.
+
+# 2  ¿Qué es un programa?
+#   En su forma más básica, como una secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo.
+
+# 3 ¿Cuál es la diferencia entre un compilador y un intérprete?
+#   Un intérprete lee el código fuente de los programas tal y como ha sido escrito por el programador, lo analiza, e interpreta sus instrucciones sobre la marcha. Un compilador necesita que le entreguen el programa completo en un fichero, y luego ejecuta un proceso para traducir el código fuente de alto nivel a código máquina, tras lo cual coloca ese código máquina resultante dentro de otro fichero para su ejecución posterior. 
+
+# 4  ¿Cuál de los siguientes contiene “código máquina”?
+#   El intérprete de Python.
+
+# 5  ¿Qué está mal en el siguiente código?
+#   el primt esta mal escrito deberi ser Print.
+
+# 6 ¿En qué lugar del computador queda almacenada una variable, como en este caso “X”, después de ejecutar la siguiente línea de Python?
+#   Dispositivos de Entrada
+
+# 7 ¿Qué mostrará en pantalla el siguiente programa?
+#   44
+
+# 8  Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana: (1) Unidad central de procesamiento, (2) Memoria principal, (3) Memoria secundaria, (4) Dispositivos de entrada, y (5) Dispositivos de salida.
+#   1. el cerebro, 2. memoria a corto plazo, 3. memoria de largo plazo, 4. los sentidos, 5. acciones
+
+# 9  ¿Cómo puedes corregir un “Error de sintaxis”?
+#   leer detenidamente el codigo.
